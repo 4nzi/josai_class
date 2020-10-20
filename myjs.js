0477dd14909330d9cdc60ce9d24937167916eaa4
4nzi <zm17328@josai.ac.jp>
2020-10-20 01:48:57 +0000

    message

--- myjs.js
+++ myjs.js
@@ -1,17 +1,26 @@
 //alert('Hello, Anzi!');
 // alert(1+1);
-alert(Math.sqrt(1+1));
-alert(Math.sqrt(2) + Math.sqrt(3));
-alert(Math.floor(1/0));
-alert(Math.floor(0/0));
-alert(2+3);
-alert('2'+'3');
-alert(Number('2')+Number('3'));
-alert('2'+3)
-alert(Number('2')+3)
+//alert(Math.sqrt(1+1));
+//alert(Math.sqrt(2) + Math.sqrt(3));
+//alert(Math.floor(1/0));
+//alert(Math.floor(0/0));
+//alert(2+3);
+//alert('2'+'3');
+//alert(Number('2')+Number('3'));
+//alert('2'+3)
+//alert(Number('2')+3)
+var who = "world";
 function sayhello() {
-alert('Hello, Function!');
+    alert('Hello, ' + who + '!');
+}
+function taro() {
+    who = "太郎" ;
+    alert(who + 'が来た');
+}
+function hanako() {
+    who = "花子" ;
+    alert(who + 'が来た');
 }
 function saygoodbye() {
-alert('Goodbye');
+    alert('Goodbye');
 }
