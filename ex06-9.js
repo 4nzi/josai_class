aeeb4ae4de443c5318895d81ffa0ef3de432e3c3
4nzi <zm17328@josai.ac.jp>
2020-10-27 03:50:10 +0000

    message

---
+++ ex06-9.js
@@ -0,0 +1,17 @@
+function okikae() {
+    document.getElementById("ichigo").src="./image/ichigo2.jpg";
+}
+
+function okikae2() {
+    document.getElementById('ichigo').src="image/ichigo.jpg";
+} 
+
+function okikae3() {
+   document.getElementById('ichigo2').src="image/ichigo2.jpg"; 
+   setTimeout("modoru()",3000);
+}
+
+function modoru() {
+    document.getElementById('ichigo2').src="image/ichigo.jpg";
+
+}
--- myjs.js
+++ myjs.js
@@ -9,18 +9,3 @@
 //alert(Number('2')+Number('3'));
 //alert('2'+3)
 //alert(Number('2')+3)
-var who = "world";
-function sayhello() {
-    alert('Hello, ' + who + '!');
-}
-function taro() {
-    who = "太郎" ;
-    alert(who + 'が来た');
-}
-function hanako() {
-    who = "花子" ;
-    alert(who + 'が来た');
-}
-function saygoodbye() {
-    alert('Goodbye');
-}
