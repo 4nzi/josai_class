d7fa1ef0a1e63e7e83d6ea4cd92b5a49e1282901
4nzi <zm17328@josai.ac.jp>
2020-12-15 01:32:00 +0000

    1215

--- ex06-5.js
+++ ex06-5.js
@@ -2,14 +2,21 @@ var who65 = "world";
 function sayhello65() {
     alert('Hello, ' + who65 + '!');
 }
+
+function saygoodbye() {
+    alert('Goodbye ' );
+}
+
 function taro65() {
     who65 = "太郎" ;
     alert(who65);
 }
+
 function hanako65() {
     who65 = "花子" ;
     alert(who65);
 }
+
 function gakita() {
     alert("が来た")
 }
